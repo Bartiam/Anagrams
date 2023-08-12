af00c4eb5f66e1144a7974eef644d7e9bf641cce
Andrey Golyachenko <leswar@yandex.ru>
2023-08-12 21:51:00 +0500

    Complite the work.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,56 @@
 #include <iostream>
+#include <string>
+#include <Windows.h>
+#include <map>
+
+bool is_correct_input_data(const std::string& line)
+{
+	for (int i = 0; i < line.length(); ++i)
+		if ((line[i] < 'a' || line[i] > 'z') && (line[i] < 'à' || line[i] > 'ÿ')) return false;
+
+	return true;
+}
+
+bool is_anagram(const std::string& firstLine, const std::string& secondLine,
+	std::map<char, char>& anagram)
+{
+	int currentSize;
+
+	if (firstLine.length() != secondLine.length()) return false;
+
+	for (int i = 0; i < firstLine.length(); ++i)
+		anagram.emplace(firstLine[i], firstLine[i]);
+
+	currentSize = anagram.size();
+
+	for (int i = 0; i < secondLine.length(); ++i)
+		anagram.emplace(secondLine[i], secondLine[i]);
+
+	if (anagram.size() == currentSize) return true;
+	else return false;
+}
 
 int main()
 {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	setlocale(LC_ALL, "Russian");
+	std::string firstLine;
+	std::string secondLine;
+	std::map<char, char> anagram;
+
+	std::cout << "Enter the first word: ";
+	std::getline(std::cin, firstLine);
+		
+	std::cout << "Enter the second word: ";
+	std::getline(std::cin, secondLine);
+
+	if (is_correct_input_data(firstLine) &&
+		is_correct_input_data(secondLine))
+	{
+		std::cout << is_anagram(firstLine, secondLine, anagram) << std::endl;
+	}
+	else std::cerr << "Error! Incorrect input data. " << std::endl;
+
 	return 0;
 }
